06a3e24f4edac58c90866cf7ceec3302ccc6f057
kalypso2 <sam.ebaugh@gmail.com>
2026-03-28 17:50:46 -0400

    merged branches

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from typing import Optional
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from dotenv import load_dotenv
+load_dotenv("config/keys.env")
 load_dotenv()
 
 from flask import Flask, Response, jsonify, request
